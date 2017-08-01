c79ea8d003302d58825c2913320629ddcb5d6ecb
Leslie <leslie.kep@gmail.com>
2017-08-01 12:20:37 +0200

    initial

---
+++ ambition_reference/tests/test_reference.py
@@ -0,0 +1,10 @@
+from django.test import TestCase, tag
+
+from edc_reference.site import site_reference_configs
+from pprint import pprint
+
+
+class TestReference(TestCase):
+
+    def test_(self):
+        pprint(site_reference_configs.registry)
