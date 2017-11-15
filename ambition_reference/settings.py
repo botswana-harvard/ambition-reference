214831dca186a9552479d1c7663d2166747922e2
erikvw <ew2789@gmail.com>
2017-11-15 11:23:50 +0200

    fix tests

--- ambition_reference/settings.py
+++ ambition_reference/settings.py
@@ -119,7 +119,6 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'ambition_reference', 'static')
-KEY_PATH = os.path.join(BASE_DIR, 'crypto_fields')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
