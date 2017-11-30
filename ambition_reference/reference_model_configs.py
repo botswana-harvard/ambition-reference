52e78a564c4d9592eb7b1340753a7d1b98861d29
Leslie <leslie.kep@gmail.com>
2017-11-30 09:12:03 +0200

    remove ae references

--- ambition_reference/reference_model_configs.py
+++ ambition_reference/reference_model_configs.py
@@ -5,10 +5,7 @@ site_reference_configs.register_from_visit_schedule(
     site_visit_schedules=site_visit_schedules)
 
 configs = {
-    'ambition_subject.adverseevent': ['ae_severity_grade', 'ae_cm_recurrence'],
-    'ambition_subject.bloodresult': ['abnormal_results_in_ae_range'],
     'ambition_subject.prnmodel': [
-        'adverse_event', 'adverse_event_tmg', 'adverse_event_followup',
         'microbiology', 'radiology', 'protocol_deviation', 'blood_result',
         'lumbar_puncture', 'recurrence_symptom', 'death_report'],
     'ambition_subject.deathreport': ['cause_of_death'],
