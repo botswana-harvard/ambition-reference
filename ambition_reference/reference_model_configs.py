bbe74bd318c3f96d00f890c96d5425bd190892fb
Leslie <leslie.kep@gmail.com>
2017-12-03 15:22:09 +0200

    comment unrequired reference model confings

--- ambition_reference/reference_model_configs.py
+++ ambition_reference/reference_model_configs.py
@@ -6,14 +6,14 @@ site_reference_configs.register_from_visit_schedule(
 
 configs = {
     'ambition_subject.prnmodel': ['blood_result', 'microbiology', 'radiology', 'lumbar_puncture'],
-    'ambition_subject.deathreport': ['cause_of_death'],
-    'ambition_subject.deathreporttmg1': ['cause_of_death'],
+    #     'ambition_subject.deathreport': ['cause_of_death'],
+    #     'ambition_subject.deathreporttmg1': ['cause_of_death'],
     'ambition_subject.education': ['household_head'],
     'ambition_subject.patienthistory': ['cd4_date', 'viral_load_date'],
-    'ambition_subject.protocoldeviationviolation': ['deviation_or_violation'],
+    #     'ambition_subject.protocoldeviationviolation': ['deviation_or_violation'],
     'ambition_subject.medicalexpenses': ['care_before_hospital'],
-    'ambition_subject.recurrencesymptom': ['patient_readmitted', 'lp_completed'],
-    'ambition_subject.studyterminationconclusion': ['termination_reason'],
+    #     'ambition_subject.recurrencesymptom': ['patient_readmitted', 'lp_completed'],
+    #     'ambition_subject.studyterminationconclusion': ['termination_reason'],
     'ambition_subject.week16': ['patient_alive'],
 }
 
