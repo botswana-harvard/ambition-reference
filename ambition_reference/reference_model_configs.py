142731bd4a7e6682177dc80c5d06056aaadf2a3e
Leslie <leslie.kep@gmail.com>
2017-12-12 16:49:19 +0200

    update prn reference fields

--- ambition_reference/reference_model_configs.py
+++ ambition_reference/reference_model_configs.py
@@ -5,7 +5,9 @@ site_reference_configs.register_from_visit_schedule(
     site_visit_schedules=site_visit_schedules)
 
 configs = {
-    'ambition_subject.prnmodel': ['blood_result', 'microbiology', 'radiology', 'lumbar_puncture'],
+    'ambition_subject.prnmodel': [
+        'blood_result', 'microbiology', 'radiology', 'lumbar_puncture',
+        'cd4', 'viral_load'],
     #     'ambition_subject.deathreport': ['cause_of_death'],
     #     'ambition_subject.deathreporttmg1': ['cause_of_death'],
     'ambition_subject.education': ['household_head'],
